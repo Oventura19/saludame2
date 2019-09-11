1acde39750e410da23b5e709722dc102372f827f
Oventura19 <47031203+Oventura19@users.noreply.github.com>
2019-09-10 19:40:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 
 app.get('/makers/:nombre', (req, res) => {
+    var name = req.params.nombre;
+
+    function MaysPrimera(name) {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
     if (req.params.nombre) {
-        res.send("<h1>Hola " + req.params.nombre.toLowerCase() + "!</h1>");
+        res.send("<h1>Hola " + MaysPrimera(req.params.nombre) + "!</h1>");
+
     } else {
         res.send("<h1>Hola desconocido!</h1>");
     }
