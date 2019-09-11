041533d0fffb26b864c82323eb63184c2c636c3b
Oventura19 <47031203+Oventura19@users.noreply.github.com>
2019-09-10 19:18:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/makers/:nombre', (req, res) => {
     if (req.params.nombre) {
-        res.send("<h1>Hola " + req.params.nombre.toUpperCase() + "!</h1>");
+        res.send("<h1>Hola " + req.params.nombre.string.charAt(0).toUpperCase() + "!</h1>");
     } else {
         res.send("<h1>Hola desconocido!</h1>");
     }
